090da99bdfe87a41f394c27854ebae5869dcf5db
Samuel Rodriguez <sar325@cornell.edu>
2020-07-20 16:42:22 -0400

    More edits

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -9,10 +9,12 @@ function hyp2Cliq(dataFile::String, dataFile::String)
                 a hypergraph
 
     Parameter: File containing hypergraph
-    Precondition: Two
+    Precondition:
     =#
 
-    #FileReading Hypergraph (if we have to Files)
+    #FileReading Hypergraph (if we have two Files)
+
+    hyper # Hypergraph Variable
 
     # Hypergraph (Converting if hyperedge is line by line)
     # line2vec(line) = parse.(Int, split(line, '\t' ))
@@ -20,7 +22,18 @@ function hyp2Cliq(dataFile::String, dataFile::String)
     #         keep = false) if !isempty(line)]
 
     # Conversion to clique
-    clique=Int64[]
+    clique = Vector{Array{Int64, 1}}
+
+
+
+    n = size(hyper, 1)
+    for k = 1:n
+        m = size(hyper, 2)
+        temp=zeros(m)
+        for l=1:m
+            temp[l]
+    end
+
 
     # Once we have clique, this makes the adjency matrix of clique
     A = max.(clique, clique')
