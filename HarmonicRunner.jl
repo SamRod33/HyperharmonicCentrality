d548d1296e176d45cb33b17dd25af265c91df33a
Samuel Rodriguez <sar325@cornell.edu>
2020-07-20 16:42:01 -0400

    Edits in the harmonic code 7/20/20

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -3,28 +3,26 @@ using Random
 using  PyCall
 nx=pyimport("networkx")
 
-function hyp2Cliq(dataFile::String)
+function hyp2Cliq(dataFile::String, dataFile::String)
     #=
     Returns: clique representing the projected graph of
                 a hypergraph
 
     Parameter: File containing hypergraph
-    Precondition: File that contains a set of nodes
-                    in the Hyperedge on one line,
-                    and each line representing a hyperedge
+    Precondition: Two
     =#
 
-    # Hypergraph
-    line2vec(line) = parse.(Int, split(line, '\t' ))
-    hyper=[line2vec(line) for line in eachline(dataFile;
-            keep = false) if !isempty(line)]
+    #FileReading Hypergraph (if we have to Files)
+
+    # Hypergraph (Converting if hyperedge is line by line)
+    # line2vec(line) = parse.(Int, split(line, '\t' ))
+    # hyper=[line2vec(line) for line in eachline(dataFile;
+    #         keep = false) if !isempty(line)]
+
     # Conversion to clique
     clique=Int64[]
 
     # Once we have clique, this makes the adjency matrix of clique
-    n = # Fill in
-    data = sparse(convert(Vector{Int64}, data[:,1]),
-               convert(Vector{Int64}, data[:,2]), 1, n, n)
-    data = max.(data, data')
+    A = max.(clique, clique')
 
 end
