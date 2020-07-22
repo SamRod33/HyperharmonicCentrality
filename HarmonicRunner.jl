b115ed948479b75998062c60afd4d9172997868d
mrampersaud <63891045+mrampersaud@users.noreply.github.com>
2020-07-22 14:56:06 -0400

    added some filereading

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -3,7 +3,7 @@ using Random
 using  PyCall
 nx=pyimport("networkx")
 
-function hyp2Cliq(dataFile::String, dataFile::String)
+function hyp2Cliq(dataFile1::String, dataFile2::String)
     #=
     Returns: clique representing the projected graph of
                 a hypergraph
@@ -11,9 +11,27 @@ function hyp2Cliq(dataFile::String, dataFile::String)
     Parameter: File containing hypergraph
     Precondition:
     =#
-
-
     #FileReading Hypergraph (if we have two Files)
+    
+    #first file's line number* is its position in the matrix
+    #the number on that line* is how many nodes are in the edge
+    #go into second file and take that many nodes from last (keep track of
+    #position last taken from)
+
+    #final matrix like this:
+    # hyper = [ [nodes in hyperedge1]; [nodes in hyperedge2]; ... ; [nodes in hyperedge n] ]
+
+    edges = readdlm(dataFile1)
+    nodes = readdlm(datafile2)
+    rows = size(nodes,1) #number of hyperedges / rows in hypergraph matrix
+
+    for line in 1:rows
+        nodes = edges[line] #number of nodes to take from other file
+
+
+
+    #first crate a matrix and zero it out?
+    hyper = zeros([T=Float64,] Int64::2,3)
 
     hyper # Hypergraph Variable
 
