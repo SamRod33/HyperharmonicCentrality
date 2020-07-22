8110e3a9995730200f50fa9c87967bfd00fdeae5
mrampersaud <63891045+mrampersaud@users.noreply.github.com>
2020-07-22 16:29:04 -0400

    Merge branch 'master' of https://github.com/SamRod33/HyperharmonicCentrality

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -9,8 +9,12 @@ function hyp2Cliq(dataFile1::String, dataFile2::String)
                 a hypergraph
 
     Parameter: File containing hypergraph
+<<<<<<< HEAD
     Precondition: files exist and are in the same folder, first file lists the
     hyperedges and second file lists the nodes
+=======
+    Precondition: Files exist and ar in the same folder
+>>>>>>> 20e388b0a2c945b422a382d0ccede870c15e40df
     =#
     #FileReading Hypergraph (if we have two Files)
 
@@ -23,11 +27,16 @@ function hyp2Cliq(dataFile1::String, dataFile2::String)
     # hyper = [ [nodes in hyperedge1]; [nodes in hyperedge2]; ... ;[nodes in hyperedge n] ]
 
     edges = readdlm(dataFile1)
+<<<<<<< HEAD
     nodes = readdlm(dataFile2)
+=======
+    nodes = readdlm(datafile2)
+>>>>>>> 20e388b0a2c945b422a382d0ccede870c15e40df
     numedges = size(edges,1) #number of hyperedges / rows in hypergraph matrix
     numnodes = size(nodes,1) #total number of hypernodes
 
 
+<<<<<<< HEAD
     #initialize hypergraph as an array of arrays
     hyper = hyper=[[1,2]]
 
@@ -40,6 +49,19 @@ function hyp2Cliq(dataFile1::String, dataFile2::String)
         push!(hyper, Edge) #place hyperedge array in overall hypergraph
     end
     hyper=hyper[2:end]
+=======
+    #first crate a hypergraph array and zero it out
+    hyper = zeros(Int64, numedges ,1)
+
+    #go through each line
+
+    i = 1 #line pos we're at in second file
+    for line in 1:numedges
+        take = edges[line] #number of nodes to take from other file
+        Edge = nodes[i:i+take] #makes an array representing hyperedge with its nodes
+        i += take #update line postion in second file
+        hyper[line] = Edge #place hyperedge array in overall hypergraph
+>>>>>>> 20e388b0a2c945b422a382d0ccede870c15e40df
 
 
     # Hypergraph (Converting if hyperedge is line by line)
