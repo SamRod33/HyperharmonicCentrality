740495fb2bdd52a40e39e73d10406c0fe934c7cd
mrampersaud <63891045+mrampersaud@users.noreply.github.com>
2020-07-22 16:34:42 -0400

    filereading

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -7,8 +7,10 @@ function hyp2Cliq(dataFile1::String, dataFile2::String)
     #=
     Returns: clique representing the projected graph of
                 a hypergraph
+
     Parameter: File containing hypergraph
-    Precondition: Files exist and ar in the same folder
+    Precondition: files exist and are in the same folder, first file lists the
+    hyperedges and second file lists the nodes
     =#
     #FileReading Hypergraph (if we have two Files)
 
@@ -21,22 +23,23 @@ function hyp2Cliq(dataFile1::String, dataFile2::String)
     # hyper = [ [nodes in hyperedge1]; [nodes in hyperedge2]; ... ;[nodes in hyperedge n] ]
 
     edges = readdlm(dataFile1)
-    nodes = readdlm(datafile2)
+    nodes = readdlm(dataFile2)
     numedges = size(edges,1) #number of hyperedges / rows in hypergraph matrix
     numnodes = size(nodes,1) #total number of hypernodes
 
 
-    #first crate a hypergraph array and zero it out
-    hyper = zeros(Int64, numedges ,1)
+    #initialize hypergraph as an array of arrays
+    hyper = hyper=[[1,2]]
 
     #go through each line
-
     i = 1 #line pos we're at in second file
     for line in 1:numedges
         take = edges[line] #number of nodes to take from other file
-        Edge = nodes[i:i+take] #makes an array representing hyperedge with its nodes
-        i += take #update line postion in second file
-        hyper[line] = Edge #place hyperedge array in overall hypergraph
+        Edge = nodes[i:(i-1)+Int64(take)] #makes an array representing hyperedge with its nodes
+        i += Int64(take) #update line postion in second file
+        push!(hyper, Edge) #place hyperedge array in overall hypergraph
+    end
+    hyper=hyper[2:end]
 
 
     # Hypergraph (Converting if hyperedge is line by line)
@@ -44,21 +47,25 @@ function hyp2Cliq(dataFile1::String, dataFile2::String)
     # hyper=[line2vec(line) for line in eachline(dataFile;
     #         keep = false) if !isempty(line)]
 
-    # Conversion to clique
-    clique = Vector{Array{Int64, 1}}
+
+#     # Conversion to clique
+#     clique = Vector{Array{Int64, 1}}
+#
+#
+#
+#     n = size(hyper, 1)
+#     for k = 1:n
+#         m = size(hyper, 2)
+#         temp=zeros(m)
+#         for l=1:m
+#             temp[l]
+#     end
+#
+#
+#     # Once we have clique, this makes the adjency matrix of clique
+#     A = max.(clique, clique')
+#
+ end
 
 
-
-    n = size(hyper, 1)
-    for k = 1:n
-        m = size(hyper, 2)
-        temp=zeros(m)
-        for l=1:m
-            temp[l]
-    end
-
-
-    # Once we have clique, this makes the adjency matrix of clique
-    A = max.(clique, clique')
-
-end
+hyp2Cliq("email-Enron/email-Enron-nverts.txt", "email-Enron/email-Enron-simplices.txt")
