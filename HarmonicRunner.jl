11ef4d5551a3593947fd7657dd49f52fea70e5ab
Samuel Rodriguez <sar325@cornell.edu>
2020-07-22 14:19:48 -0400

    Removed hello

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -11,8 +11,7 @@ function hyp2Cliq(dataFile::String, dataFile::String)
     Parameter: File containing hypergraph
     Precondition:
     =#
-    
-    hello
+
 
     #FileReading Hypergraph (if we have two Files)
 
