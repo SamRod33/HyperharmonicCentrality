107fef110d1dd56ee6da44e9a1a9f2493c25f6c5
Samuel Rodriguez <sar325@cornell.edu>
2020-07-31 14:37:37 -0400

    Fixed weighted harmonic centrality code and drew its visualization

--- HarmonicRunner.jl
+++ HarmonicRunner.jl
@@ -81,6 +81,57 @@ function convertToHype(dataFile1::String, dataFile2::String)
     # Once we have clique, this makes the adjency matrix of clique
  end
 
+ function convertToHype_Weighted(dataFile1::String, dataFile2::String)
+     #=
+     Returns: clique representing the projected graph of
+                 a hypergraph without the relabelling of nodes
+                 so that we can keep weights
+
+     Parameter: File containing hypergraph
+     Precondition: files exist and are in the same folder, first file lists the
+     hyperedges and second file lists the nodes
+     =#
+     #FileReading Hypergraph (if we have two Files)
+
+     #first file's line number* is its position in the matrix
+     #the number on that line* is how many nodes are in the edge
+     #go into second file and take that many nodes from last (keep track of
+     #position last taken from)
+
+     #final array like this:
+     # hyper = [ [nodes in hyperedge1]; [nodes in hyperedge2]; ... ;[nodes in hyperedge n] ]
+
+     edges = readdlm(dataFile1)
+     nodes = readdlm(dataFile2)
+     numedges = size(edges,1) #number of hyperedges / rows in hypergraph matrix
+     numnodes = size(nodes,1) #total number of hypernodes
+
+     #initialize hypergraph as an array of arrays
+     hyper = hyper=[[1,2]]
+
+     #go through each line
+     i = 1 #line pos we're at in second file
+     for line in 1:numedges
+         take = edges[line] #number of nodes to take from other file
+         if take > 1
+             Edge = nodes[i:(i-1)+Int64(take)] #makes an array representing hyperedge with its nodes
+             push!(hyper, Edge) #place hyperedge array in overall hypergraph
+         end
+         i += Int64(take) #update line postion in second file
+     end
+     hyper=hyper[2:end]
+
+     # Hypergraph (Converting if hyperedge is line by line)
+     # line2vec(line) = parse.(Int, split(line, '\t' ))
+     # hyper=[line2vec(line) for line in eachline(dataFile;
+     #         keep = false) if !isempty(line)]
+
+
+
+     return unique(hyper)
+     # Once we have clique, this makes the adjency matrix of clique
+  end
+
 
 function hype2Clique(hyper::Vector{Array{Int64, 1}})
     # Conversion to clique
@@ -138,7 +189,15 @@ end
 
 function getHypergraph()
     #= Returns Hypergraph from files Enron =#
-    hypergraph = convertToHype("email-Enron/email-Enron-nverts.txt", "email-Enron/email-Enron-simplices.txt")
+    hypergraph = convertToHype("email-Enron/email-Enron-nverts.txt",
+    "email-Enron/email-Enron-simplices.txt")
+    return hypergraph
+end
+
+function getHypergraph_Weighted()
+    #= Returns Hypergraph from files Enron Weighted Version =#
+    hypergraph = convertToHype_Weighted("email-Enron/email-Enron-nverts.txt",
+    "email-Enron/email-Enron-simplices.txt")
     return hypergraph
 end
 
@@ -163,7 +222,7 @@ end
 
 function getAdjMatrix_Weighted()
     #= Returns clique that is supposed to be weighted =#
-    hypergraph = getHypergraph()
+    hypergraph = getHypergraph_Weighted()
     clique = hype2Clique(hypergraph)
     A1 = cliqueToAdjMatrix_Weighted(clique)
     A = Matrix(A1)
@@ -218,51 +277,20 @@ cCentScoresNorm = cCentScores/ maximum(cCentScores)
 norm(hCentScoresNorm-cCentScoresNorm,2)
 ### L_2 Norm --> 0.1869 result on a normalized scale (i.e from 0-1 scale) ###
 
-########################################################################
-#---------------------HAS YET TO WORK----------------------------------#
-########################################################################
-function rank_corrbetweenness(name::Any,dataset::Any, index_start::Int64)
-    cec_c=dataset[:,1]
-    n=length(cec_c)
-    cec_c[cec_c]=[i for i in 1:n]
-    hec_c=dataset[:,2]
-    hec_c[hec_c]=[i for i in 1:n]
-    start = log10(index_start - 1)
-    finish = log10(length(cec_c) - 1)
-    ran = [convert(Int64, round(v)) for v in 10 .^ range(start, stop=finish, length=500)]
-    sp = sortperm(cec_c)
-    cec_c, hec_c= cec_c[sp], hec_c[sp]
-    cec_hec = [corspearman(cec_c[(end-s):end], hec_c[(end-s):end]) for s in ran]
-    sp = sortperm(hec_c)
-    cec_c, hec_c = cec_c[sp], hec_c[sp]
-    hec_cec = [corspearman(hec_c[(end-s):end], cec_c[(end-s):end]) for s in ran]
-    clf()
-    xs = collect(ran) .+ 1
-    semilogx(xs, cec_hec, linestyle="-",  lw=1,    label="TWDB-H2NB")
-    semilogx(xs, hec_cec, linestyle=":",  lw=2.25, label="H2NB-TWDB")
-    fsz = 20
-    ax = gca()
-    ax.tick_params(axis="both", labelsize=fsz-2, length=7, width=1.5)
-    ax.tick_params(axis="x",    which="minor", length=4, width=1)
-    legend(fontsize=fsz-10, frameon=false)
-    title("$name", fontsize=fsz)
-    xlabel("Number of top-ranked elements", fontsize=fsz)
-    ylabel("Spearman’s rank corr. coeff.", fontsize=fsz)
-    savefig("6241-spearman-$name-rankcorr.png", bbox_inches="tight")
-end
-rank_corrbetweenness("B", h, 10)
-
-##____IDEA_____##
-# Rank top ten closness and harmonic (normalized)
-# Generalize for each rank afterwards (spearmans correlation coefficient)
-##_____________##
 
 ##############################################################################
 ######-------------- WHAT IF WE DIDNT MAKE ALL WEIGHTS ONE ------------#######
 ##############################################################################
 hCentW=getHarmonicCentrality(getAdjMatrix_Weighted())
 ordered_hCentW=sort(hCentW)
+nMax = maximum(ordered_hCentW)[1]
+for n=0:(nMax-1)
+    if ordered_hCentW[n] == 0
+        delete!(ordered_hCentW, n)
+    end
+end
 hCentWScores = [t[2] for t in ordered_hCentW]
+maximum(hCentWScores)
 hCentWScoresNorm = hCentWScores / maximum(hCentWScores)
 
 hCentNotW=getHarmonicCentrality(getAdjMatrix())
@@ -271,9 +299,9 @@ hCentNotWScores = [t[2] for t in ordered_hCentNotW]
 hCentNotWScoresNorm = hCentNotWScores / maximum(hCentNotWScores)
 
 norm(hCentNotWScoresNorm-hCentWScoresNorm,2)
-# L_2 Norm turned out to be 0... I don't know what to make of this
-#           the Adjacency Matrix for weight included had a max weight of 2
-#           on some nodes, not sure if the renaming of nodes affected this...
+# L_2 Norm turned out to be 1.7537 -- Weighted harmonic centrality and
+#                                     Unweighted harmonic centrality are
+#                                     VERY different
 
 
 #############################################################################
@@ -293,7 +321,7 @@ function graphHarmonicNetwork(network::Array{Int64, 2})
     nodefill = [RGBA(i,0.0,0.0,i) for i in cent]
     layout=(args...)->spring_layout(args...; C=35)
     p= gplot(g, layout=layout, nodefillc=nodefill,
-            edgestrokec=colorant"grey", nodelabel = 1:143,
+            edgestrokec=colorant"grey", nodelabel = 1:148,
             NODELABELSIZE=3, NODESIZE=0.040)
     draw(PNG("CliqueHarmonic_Weighted.png", 16cm, 16cm), p)
 end
